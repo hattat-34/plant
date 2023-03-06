4915b1a9a462327e40f06b8fedc67dec3574aee3
Nurullah Demir <ndemir@paymix.eu>
2023-03-06 18:34:05 +0300

    Refactor

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,7 +1,21 @@
 import PaywallCard from './PaywallCard';
 import PlantButton from './PlantButton';
+import PlantCard from './PlantCard';
 import PlantText from './PlantText';
+import PremiumButton from './PremiumButton';
 import PremiumOption from './PremiumOption';
+import QuestionCard from './QuestionCard';
 import SafeView from './SafeView';
+import Search from './Search';
 
-export {SafeView, PlantText, PlantButton, PaywallCard, PremiumOption};
+export {
+  SafeView,
+  PlantText,
+  PlantButton,
+  PaywallCard,
+  PremiumOption,
+  Search,
+  PremiumButton,
+  QuestionCard,
+  PlantCard,
+};
