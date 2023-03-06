fd6eb05689f252f09561cfe03dc93c46791ae9b5
Nurullah Demir <ndemir@paymix.eu>
2023-03-06 18:32:48 +0300

    React store

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './Navigation/RootNavigator';
 import Theme from './Styles/Theme';
+import {Provider} from 'react-redux';
+import store from './Store';
 
 const PlantApp = () => {
   return (
-    <NavigationContainer theme={Theme.Light}>
-      <RootNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer theme={Theme.Light}>
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
